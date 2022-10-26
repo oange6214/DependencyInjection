542682c1342d830fc043ca804a3db5cf5edfd1ad
jed <oange6214@gmail.com>
2022-10-26 21:18:11 +0800

    Add ConsoleUI Demo

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+internal class Program
+{
+    /// <summary>
+    /// 依賴注入標準設置（Loggin、Settings）
+    /// </summary>
+    private static void Main(string[] args)
+    {
+        // 設置 appsettings.json
+        var builder = new ConfigurationBuilder();
+        BuildConfig(builder);
+
+        // 設置 Log
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Build())
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        Log.Logger.Information("Application Starting");
+
+        // 設置 Host（配置 Serilog）
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                services.AddTransient<IGreetingService, GreetingService>();
+            })
+            .UseSerilog()
+            .Build();
+
+        var svc = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
+        svc.Run();
+    }
+
+    static void BuildConfig(IConfigurationBuilder builder)
+    {
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddEnvironmentVariables();
+    }
+}
--- README.md
+++ README.md
@@ -8,13 +8,11 @@
 
 	Moq
 
-	
+概述：
 
 從零開始實作，如何建立「建構函數注入」的專案，並如何做 Unit Test。
 
-
-
-References
+References：
 
 https://www.youtube.com/watch?v=2rv-lcqW1tM
 
@@ -27,9 +25,31 @@ https://www.youtube.com/watch?v=2rv-lcqW1tM
 	Serilog.AspNetCore
 
 
+概述：
+
 如何建立 Blazor Server，進行配置和使用依賴注入（Dependency Injection）
 
-
-References
+References：
 
 https://www.youtube.com/watch?v=Hhpq7oYcpGE
+
+
+# ConsoleUI
+
+套件使用：
+
+	Serilog.Sinks.Console
+
+	Serilog.Settings.Configuration
+
+	Serilog.Extensions.Hosting
+
+	Microsoft.Extensions.Extensions
+
+概述：
+
+使用 appsettings.json 設置依賴注入配置和在控制檯應用程式中使用 Serlog 進行日誌記錄。
+
+References：
+
+https://www.youtube.com/watch?v=GAOCe-2nXqc
